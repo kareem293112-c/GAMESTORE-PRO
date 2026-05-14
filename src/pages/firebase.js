44a50abd1c07c0574bfbb10da5e7a39e1743516d
kareem293112-c <kareem293112@gmail.com>
2026-05-14 19:26:31 +0300

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// قراءة الإعدادات من المتغيرات البيئية التي وضعتها في Render
+// قراءة الإعدادات من متغيرات البيئة التي وضعتها في Render
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -14,6 +14,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
+// تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
 
 // تصدير الخدمات لاستخدامها في بقية المشروع
